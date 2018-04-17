313cb4d5778a5974b302b2989a103f4e604ed24e
dimajkezh <dimajkezh@gmail.com>
2018-04-17 18:57:30 +0300

    fc

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$('.ipad_items').slick({
+  		slidesToShow: 1,
+  		slidesToScroll: 1,
+  		arrows: false,
+  		fade: true,
+ 		asNavFor: '.partner_head_items'
+	});
+
+	$('.partner_head_items').slick({
+  		slidesToShow: 1,
+  		slidesToScroll: 1,
+  		asNavFor: '.ipad_items',
+ 		prevArrow: $('.prev'),
+  		nextArrow: $('.next'),
+	});
+});
