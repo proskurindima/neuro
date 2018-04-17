26de3f36f9387e75b5136e7e2ff765ef021d7856
dimajkezh <dimajkezh@gmail.com>
2018-04-18 01:33:54 +0300

    fc

--- js/script.js
+++ js/script.js
@@ -17,10 +17,23 @@ $('.ipad_items').slick({
 //Функция Popup
     function show(state){
             document.getElementById('window').style.display = state;            
-            document.getElementById('wrap').style.display = state;             
+            document.getElementById('wrap').style.display = state;  
     }
+$(document).ready(function(){
+	$('.ai_item_button').click(function(){
+		$('#wrap').fadeIn(400);
+		$('#window').fadeIn(400);
+	});
+	$('.close').click(function(){
+		$('#wrap').fadeOut(400);
+		$('#window').fadeOut(400);
+	});
+	$('#wrap').click(function(){
+		$('#wrap').fadeOut(400);
+		$('#window').fadeOut(400);
+	});
 
-
+});
 $(window).scroll(function(){
     if ($(window).scrollTop() > 250) {
         $('.header_wrapper').addClass('scroll');
